fb2a0cf831be29739f64f1acac56d9b4096887cc
abelmarkus <abelmarkus@gmail.com>
2018-04-11 15:57:43 +0200

    Update vvvvid_skip_ads.js

--- vvvvid_skip_ads.js
+++ vvvvid_skip_ads.js
@@ -1,11 +1,14 @@
-javascript:(function () {
-    if (vvvvid.player == null || vvvvid.player == undefined) {
-		vvvvid.router.mainView.onDemandMainView.currentMode.showPlayerView.playerObj.initializePlayer();
-	}
-	vvvvid.advPlayer.destroy();
-	vvvvid.advPlayer = null
-	vvvvid.showActions();
-	$(document).trigger("player:showtopinfo");
-	$(document).trigger("menu:attachmenuvideoevents");
-    vvvvid.player.setPlay();
-})();
+javascript:(setInterval(
+		function () {
+				if (vvvvid.player == null || vvvvid.player == undefined) {
+					vvvvid.router.mainView.onDemandMainView.currentMode.showPlayerView.playerObj.initializePlayer();
+				}
+				vvvvid.advPlayer.destroy();
+				vvvvid.advPlayer = null
+				vvvvid.showActions();
+				$(document).trigger("player:showtopinfo");
+				$(document).trigger("menu:attachmenuvideoevents");
+				vvvvid.player.setPlay();
+		}
+		, 500)
+)();
